e00b21c29c22898f29f44703043efb53a57ef391
голубев дима <maska.l.v@mail.ru>
2024-10-03 20:06:01 +0500

    Task7

--- Tyuiu.GolubevDS.Sprint1.Task7.V18.Lib/DataService.cs
+++ Tyuiu.GolubevDS.Sprint1.Task7.V18.Lib/DataService.cs
@@ -5,7 +5,7 @@ public class DataService : ISprint1Task7V18
 {
     public double Calculate(double x, double y)
     {
-        double z = (1 + Math.Pow(Math.Sin(x + y), 2)) / (2 + Math.Abs(x - (2 * x / 1 + Math.Pow(x, 2) * Math.Pow(y, 2))))+x;
+        double z = (1 + Math.Pow(Math.Sin(x + y), 2)) / (2 + Math.Abs(x - (2 * x) / 1 + Math.Pow(x, 2) * Math.Pow(y, 2)))+x;
         return Math.Round(z, 3); ;
     }
 }
